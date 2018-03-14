9fb59af739ac14203eeb964e3cfccd3fec816384
GlooperJelly <ben.c.wells@gmail.com>
2018-03-14 15:45:19 -0500

    Added better error handling for 404 and 500

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,9 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+import logging
+from logging.handlers import RotatingFileHandler
+import os
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -11,4 +14,17 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
 
-from app import routes, models
+## DID NOT INCLUDE EMAIL ERROR SUPPORT SEE: 
+## https://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-vii-error-handling
+## IF YOU WANT TO INCLUDE LATER
+
+if not app.debug:
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler('logs/test.log', maxBytes=10240, backupCount=10)
+    file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+    app.logger.info('Test startup')
+    
+from app import routes, models, errors
